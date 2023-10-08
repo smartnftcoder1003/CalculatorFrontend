45c44ebdb41998a296424a7cf342abfdbde02d74
smartNFTCoder1003 <monathcorey@gmail.com>
2023-10-08 06:00:40 +0100

    update web3.js

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,15 +1,18 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Autocomplete, Button, Card, TextField } from '@mui/material';
+import Web3 from 'web3';
+import ContractABI from '../contracts/CalculatorContract-abi.json';
 
 
-const  Calculator: React.FC = () => {
+const  Calculator: any = () => {
   
   const [ firstVal, setFirstVal ] = React.useState<string>();
   const [ secondVal, setSecondVal ] = React.useState<string>();
   const [ result, setResult ] = React.useState<number>();
   const [ opr, setOpr ] = React.useState<string>('+');
+  const providerUrl = process.env.PROVIDERURL;
   const Operators = [
     {label: '+'},
     {label: '-'},
@@ -22,6 +25,13 @@ const  Calculator: React.FC = () => {
     console.log("Second Val: ", secondVal);
   }
 
+  useEffect(() => {
+    const web3 = new Web3(new Web3.providers.HttpProvider('http://localhsot:3000'));
+    const contractAddress = '0x71B5f6FF8FDaD48500403bEcF3413d393cA40514';
+    const contract = new web3.eth.Contract(ContractABI, contractAddress);
+    console.log(contract);
+  }, [])
+
   return (
     <Box sx={{ flexGrow: 1, marginTop: 20 }}>
       <Grid container spacing={2}>
@@ -29,11 +39,11 @@ const  Calculator: React.FC = () => {
           <TextField id="outlined-basic" label="First Value" variant="outlined" value={firstVal} onChange={(e: any) => setFirstVal(e.target.value)} />
         </Grid>
         <Grid item xs={2}>
-          <Autocomplete 
+          <Autocomplete
             disablePortal
             id="combo-box-demo"
             options={Operators}
-            renderInput={(params: any) => <TextField {...params} label="Operator" sx={{ width: 100 }} onChange={(e) => setOpr(e.target.value)} />}
+            renderInput={(params: any) => <TextField {...params} label="Operator" sx={{ width: 100 }} onChange={(e: any) => setOpr(e.target.value)} />}
           />
         </Grid>
         <Grid item xs={3}>
